eb8054f48898f943868822bc2f051165dd3c5c69
wksnders <42388072+wksnders@users.noreply.github.com>
2024-11-24 13:28:56 -0700

    bug fixes and letter look updates

---
+++ PostIt/PostIt/Assets/Scripts/Timer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText; 
+    private float timeRemaining;
+    int currentRound = 0;
+
+    void Start()
+    {
+        float roundLen;
+        currentRound = GameManager.Instance.currentRound;
+        if (!GameManager.Instance.roundNumToRoundLen.TryGetValue(currentRound, out roundLen))
+        {
+            Debug.LogWarning("GameplaySceneManager:Start roundLen not found, defaulting to 180 seconds");
+            roundLen = 180;
+        }
+        if (roundLen <= 0)
+        {
+            Debug.LogWarning("GameplaySceneManager:Start roundLen has an invalid value of " + roundLen +
+                ", defaulting to 180 seconds");
+            roundLen = 180;
+        }
+        timeRemaining = roundLen;
+        StartCoroutine(StartCountdown());
+    }
+
+    IEnumerator StartCountdown()
+    {
+        while (timeRemaining > 0)
+        {
+            UpdateTimerDisplay();
+            yield return null;
+            timeRemaining -= Time.deltaTime;
+        }
+
+        timeRemaining = 0;
+        UpdateTimerDisplay();
+        Debug.Log("Timer Finished!");
+    }
+
+    void UpdateTimerDisplay()
+    {
+        int minutes = Mathf.FloorToInt(timeRemaining / 60);
+        int seconds = Mathf.FloorToInt(timeRemaining % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
